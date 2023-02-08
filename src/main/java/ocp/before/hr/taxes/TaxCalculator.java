5359534b141c1993f0c42685598eadea90157d63
Shaikh Shahid Iqbal <35321907+nilgadia@users.noreply.github.com>
2023-02-08 03:53:25 +0300

    Update TaxCalculator.java

--- src/main/java/ocp/before/hr/taxes/TaxCalculator.java
+++ src/main/java/ocp/before/hr/taxes/TaxCalculator.java
@@ -1,6 +1,9 @@
 package ocp.before.hr.taxes;
 
 import ocp.before.hr.personnel.Employee;
+import ocp.before.hr.personnel.FullTimeEmployee;
+import ocp.before.hr.personnel.Intern;
+import ocp.before.hr.personnel.PartTimeEmployee;
 
 public class TaxCalculator {
     private final int RETIREMENT_TAX_PERCENTAGE = 10;
@@ -14,4 +17,28 @@ public class TaxCalculator {
                 (employee.getMonthlyIncome() * INCOME_TAX_PERCENTAGE) / 100;
 
     }
+
+    // The new requirement v2 can be implemented like this
+//    public double calculate(Employee employee) {
+//        int monthlyIncome = employee.getMonthlyIncome();
+//        if(employee instanceof FullTimeEmployee){
+//            return BASE_HEALTH_INSURANCE +
+//                    (employee.getMonthlyIncome() * RETIREMENT_TAX_PERCENTAGE) / 100 +
+//                    (employee.getMonthlyIncome() * INCOME_TAX_PERCENTAGE) / 100;
+//        }
+//        if(employee instanceof PartTimeEmployee){
+//            return BASE_HEALTH_INSURANCE +
+//                    (employee.getMonthlyIncome() * 10) / 100 +
+//                    (employee.getMonthlyIncome() * INCOME_TAX_PERCENTAGE) / 100;
+//        }
+//        if(employee instanceof Intern){
+//            if(monthlyIncome > 350){
+//                return 0;
+//            }else {
+//                return (monthlyIncome * INCOME_TAX_PERCENTAGE) / 100;
+//            }
+//        }
+//        return 0;
+//    }
+
 }
